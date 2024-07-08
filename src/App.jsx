89bfb4a95a00d1c62f730c44f586539f124c69e3
codesofpreeti <preetiipriyaa@gmail.com>
2024-07-08 22:59:59 +0530

    started

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import Header from "./components/Header";
+
+export default function App() {
+  return (
+    <h1 className="text-3xl font-bold p-2">
+      <Header/>
+      {/* hero
+      skills
+      projects
+      resume
+      contact
+      footer
+       */}
+    </h1>
+  )
+}
